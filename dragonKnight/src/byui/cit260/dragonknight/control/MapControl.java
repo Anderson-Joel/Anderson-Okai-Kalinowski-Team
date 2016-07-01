90ed45d0e12334308c2d32871bedc7e9a91e015f
DragonMaster <DragonMaster@joels-mac-mini.home>
2016-07-01 11:26:44 -0700

    7-1-16 - Part of Part 1.

--- dragonKnight/src/byui/cit260/dragonknight/control/MapControl.java
+++ dragonKnight/src/byui/cit260/dragonknight/control/MapControl.java
@@ -1,12 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package byui.cit260.dragonknight.control;
 
 import byui.cit260.dragonknight.model.Map;
+import byui.cit260.dragonknight.model.NPC;
 import byui.cit260.dragonknight.model.Scene;
+import citbyui.cit260.dragonKnight.exceptions.MapControlException;
+import dragonknight.DragonKnight;
+import java.awt.Point;
 
 /**
  *
@@ -34,8 +33,15 @@ public class MapControl {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    static void moveNPCToStartingLocation(Map map) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static int moveNPCToStartingLocation(NPC npc, Point coordinates)
+            throws MapControlException {
+        Map map = DragonKnight.getCurrentGame().getMap();
+        int newRow = coordinates.x-1;
+        int newColumn = coordinates.y-1;
+        
+        if (newRow < 0 || newRow >= map.NUM_ROWS || newColumn < 0 || newColumn >= map.NUM_COLS)
+            throw new MapControlException("Can not move NPC to location " + coordinates.x + ", " + coordinates.y
+            + " because that location is outside the bounds of the map.");
     }
     
 }
